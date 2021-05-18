a1300d996d5c1e9e37879c8e358fa65a27b3be36
mishaklimovich <mishaklimovich@tut.by>
2021-05-18 13:09:44 +0300

    feature/P1-1:adding dependencies in pom file

--- src/main/java/com/mastery/java/task/dto/Employee.java
+++ src/main/java/com/mastery/java/task/dto/Employee.java
@@ -5,3 +5,29 @@ public class Employee {
     private String firstName;
     private Gender gender;
 }
+    public Employee(){
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
