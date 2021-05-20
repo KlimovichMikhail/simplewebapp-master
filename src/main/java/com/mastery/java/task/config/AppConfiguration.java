d990a840a11b7b4ddc5807265106043901201097
Mikhail Klimovich <49657631+KlimovichMikhail@users.noreply.github.com>
2021-05-21 00:19:22 +0300

    feature/P1-1:change main class

--- src/main/java/com/mastery/java/task/config/AppConfiguration.java
+++ src/main/java/com/mastery/java/task/config/AppConfiguration.java
@@ -1,4 +1,12 @@
 package com.mastery.java.task.config;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
 public class AppConfiguration {
+    public static void main(String[] args){
+        SpringApplication.run(AppConfiguration.class, args);
+    }
 }
